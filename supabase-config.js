a38b25b01be372a635d36da7f02481b732f24157
Willian Murakami <murakamiwillian@hotmail.com>
2026-06-03 12:18:14 -0300

    Bump content fixes for carousel controls

--- supabase-config.js
+++ supabase-config.js
@@ -6,7 +6,7 @@ window.SUPABASE_CONFIG = {
 };
 
 (function loadFinalOverrides() {
-  const version = "20260603-final11";
+  const version = "20260603-final12";
   const stylesheet = document.createElement("link");
   stylesheet.rel = "stylesheet";
   stylesheet.href = `./final-overrides.css?v=${version}`;
