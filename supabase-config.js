5ade1995e42610ca62bf08c675e050b4b8c46116
Willian Murakami <murakamiwillian@hotmail.com>
2026-06-03 11:52:44 -0300

    Bump content fixes for updated references

--- supabase-config.js
+++ supabase-config.js
@@ -6,7 +6,7 @@ window.SUPABASE_CONFIG = {
 };
 
 (function loadFinalOverrides() {
-  const version = "20260603-final10";
+  const version = "20260603-final11";
   const stylesheet = document.createElement("link");
   stylesheet.rel = "stylesheet";
   stylesheet.href = `./final-overrides.css?v=${version}`;
