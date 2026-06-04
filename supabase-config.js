e4ad3eb36666a7a62306967b00d053d64f8d8b56
Willian Murakami <murakamiwillian@hotmail.com>
2026-06-04 13:34:22 -0300

    Bump reference link hotfix loader

--- supabase-config.js
+++ supabase-config.js
@@ -6,7 +6,7 @@ window.SUPABASE_CONFIG = {
 };
 
 (function loadFinalOverrides() {
-  const version = "20260603-final21";
+  const version = "20260603-final22";
   const stylesheet = document.createElement("link");
   stylesheet.rel = "stylesheet";
   stylesheet.href = `./final-overrides.css?v=${version}`;
