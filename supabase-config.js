26d0cd3dda615ba64bacdcc590b15b5bb422b52b
Willian Murakami <murakamiwillian@hotmail.com>
2026-06-05 13:47:25 -0300

    Ajusta respiro visual dos cases

--- supabase-config.js
+++ supabase-config.js
@@ -6,7 +6,7 @@ window.SUPABASE_CONFIG = {
 };
 
 (function loadFinalOverrides() {
-  const version = "20260605-cases2";
+  const version = "20260605-cases3";
   const stylesheet = document.createElement("link");
   stylesheet.rel = "stylesheet";
   stylesheet.href = `./final-overrides.css?v=${version}`;
